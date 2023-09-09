72ffed7be914cf060b305bdc26728d9d502cb6be
jennysol <jennyssoliver@gmail.com>
2023-09-09 11:24:38 -0300

    feat: array interfaces

--- Classes/script.js
+++ Classes/script.js
@@ -70,3 +70,31 @@ async function fetchProduct() {
     showProduct(data);
 }
 fetchProduct();
+function showCurso(cursos) {
+    cursos.forEach((curso) => {
+        let color;
+        if (curso.nivel === "iniciante") {
+            color = "blue";
+        }
+        else if (curso.nivel === "avancado") {
+            color = "red";
+        }
+        document.body.innerHTML += `
+      <div>
+        <h2 style="color: ${color}">${curso.nome}</h2>
+        <p> Horas: ${curso.horas}</p>
+        <p> Aulas : ${curso.aulas}</p>
+        <p> Tipo : ${curso.gratuito ? "Gratuito" : "Pago"}</p>
+        <p> Tags: ${curso.tags.join(", ")}</p>
+        <p> Aulas: ${curso.idAulas.join(" | ")}</p>
+      </div>
+    `;
+    });
+}
+async function fetchCursos() {
+    const response = await fetch("http://api.origamid.dev/json/cursos.json");
+    const data = await response.json();
+    console.log("Responde fetch cursos", data);
+    showCurso(data);
+}
+fetchCursos();
--- Classes/script.ts
+++ Classes/script.ts
@@ -102,3 +102,47 @@ async function fetchProduct() {
 }
 
 fetchProduct();
+
+// 2 Consult array interface
+
+interface Curso {
+  nome: string;
+  aulas: string;
+  gratuito: boolean;
+  horas: number;
+  idAulas: number[];
+  nivel: "iniciante" | "avancado";
+  tags: string[];
+}
+
+function showCurso(cursos: Curso[]) {
+  cursos.forEach((curso) => {
+    let color;
+    if (curso.nivel === "iniciante") {
+      color = "blue";
+    } else if (curso.nivel === "avancado") {
+      color = "red";
+    }
+
+    document.body.innerHTML += `
+      <div>
+        <h2 style="color: ${color}">${curso.nome}</h2>
+        <p> Horas: ${curso.horas}</p>
+        <p> Aulas : ${curso.aulas}</p>
+        <p> Tipo : ${curso.gratuito ? "Gratuito" : "Pago"}</p>
+        <p> Tags: ${curso.tags.join(", ")}</p>
+        <p> Aulas: ${curso.idAulas.join(" | ")}</p>
+      </div>
+    `;
+  });
+}
+
+async function fetchCursos() {
+  const response = await fetch("http://api.origamid.dev/json/cursos.json");
+  const data = await response.json();
+  console.log("Responde fetch cursos", data);
+
+  showCurso(data);
+}
+
+fetchCursos();
