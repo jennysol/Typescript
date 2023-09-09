2ae8459d792618829553ce430381c262e38e2d85
jennysol <jennyssoliver@gmail.com>
2023-09-09 16:34:10 -0300

    feat: add instanceof

---
+++ Classes/instanceof.js
@@ -0,0 +1,5 @@
+"use strict";
+const link = document.getElementById("origamid");
+if (link instanceof HTMLAnchorElement) {
+    link.href = link.href.replace("http://", "https://");
+}
---
+++ Classes/instanceof.ts
@@ -0,0 +1,5 @@
+const link = document.getElementById("origamid");
+
+if (link instanceof HTMLAnchorElement) {
+  link.href = link.href.replace("http://", "https://");
+}
