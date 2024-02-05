45374323a9ed30c8724d3f0af492f87a67be6c97
jennysol <jennyssoliver@gmail.com>
2024-02-05 07:25:52 -0300

    feat: function activity

---
+++ Functions/script.js
@@ -0,0 +1,14 @@
+"use strict";
+// Crie uma função que arredonda um valor passado pra ela
+// A função pode receber string ou number
+// A função deve retornar o mesmo tipo que receber
+function round(n) {
+    if (typeof n === "number") {
+        return Math.round(n);
+    }
+    else {
+        return Math.ceil(Number(n)).toString();
+    }
+}
+console.log(round("3.3"), typeof round("3.3"));
+console.log(round(2.2), typeof round(2.2));
